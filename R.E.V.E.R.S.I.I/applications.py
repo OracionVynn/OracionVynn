b46414116855f3856ceb8bcb5a95149319c406ee
OracionVynn <154253608+OracionVynn@users.noreply.github.com>
2024-02-17 07:59:11 -0400

    deleted

--- R.E.V.E.R.S.I.I/applications.py
+++
@@ -1,9 +0,0 @@
-import threading
-import subprocess
-import webbrowser
-
-x=lambda: webbrowser.open_new("http://google.com")
-t=threading.Thread(target=x)
-t.start()
-
-subprocess.Popen("C:\\windows\\System32\\calc.exe")
